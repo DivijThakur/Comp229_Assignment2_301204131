1d3a54b2e040aa38d9e82f4f8aac50d68d854b61
divij singh <divijsingh2@gmail.com>
2022-06-26 09:02:24 -0400

    first commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,13 @@
+/*
+NAME : DIVIJ THAKUR
+STUDENT ID: 301204131
+*/
+(function () {
+
+    function Start() {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+/*
+NAME : DIVIJ THAKUR
+STUDENT ID: 301204131
+*/
+module.exports = 
+{
+    "URI": "mongodb://localhost:27017/Divij",
+    //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SSecret'
+}
